b128fdabf0e47dedbc15a610cf4d57bf7fb029eb
dhk5844 <dhk5844@ajou.ac.kr>
2020-11-20 22:28:52 +0900

    Edit writing page (class, xml, resources), etc

---
+++ app/src/main/java/com/posturn/hotplace/FilterPageActivity.java
@@ -0,0 +1,14 @@
+package com.posturn.hotplace;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FilterPageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.filter_page);
+    }
+
+}
