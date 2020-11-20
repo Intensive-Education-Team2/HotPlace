4334df9b3aa19e7eb01964b6e155c9cc4fc835e7
dhk5844 <dhk5844@ajou.ac.kr>
2020-11-20 17:26:39 +0900

    Add writing page (class, xml, resources)

---
+++ app/src/main/java/com/posturn/hotplace/LoadingPageActivity.java
@@ -0,0 +1,15 @@
+package com.posturn.hotplace;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoadingPageActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading_page);
+    }
+}
