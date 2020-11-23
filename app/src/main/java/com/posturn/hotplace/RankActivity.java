e57f0e40647a2658a74e63ad822841a6914951a4
dhk5844 <dhk5844@ajou.ac.kr>
2020-11-23 09:23:05 +0900

    Add Rank page to Manifest

--- app/src/main/java/com/posturn/hotplace/RankActivity.java
+++ app/src/main/java/com/posturn/hotplace/RankActivity.java
@@ -4,16 +4,19 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-public class RecommandActivity extends AppCompatActivity {
+public class RankActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.recommand_page);
+        setContentView(R.layout.rank_page);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         TextView toolbarTitle = (TextView) findViewById(R.id.toolbar_title);
-        toolbarTitle.setText("추천 핫플레이스");
-
+        toolbarTitle.setText("핫플레이스 순위");
     }
+
 }
