b7823d06360fb3bab82e594b59daebb5cc1cb195
KHG <skidrom1@gmail.com>
2020-11-23 22:33:11 +0900

    순위 및 추천순위 페이지 수정 및 연결

---
+++ app/src/main/java/com/posturn/hotplace/MyPlaceActivity.java
@@ -0,0 +1,14 @@
+package com.posturn.hotplace;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyPlaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myplace);
+    }
+}
--- app/src/main/java/com/posturn/hotplace/RecommandActivity.java
+++ app/src/main/java/com/posturn/hotplace/RecommandActivity.java
@@ -1,9 +1,12 @@
 package com.posturn.hotplace;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class RecommandActivity extends AppCompatActivity {
 
@@ -12,8 +15,25 @@ public class RecommandActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommand_page);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();    //뒤로가기 버튼 생성
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
         TextView toolbarTitle = (TextView) findViewById(R.id.toolbar_title);
         toolbarTitle.setText("추천 핫플레이스");
+    }
 
+    //툴바 기능
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: { // 뒤로가기
+                onBackPressed();
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
