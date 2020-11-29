b377e90508f38c0e3a7daa292f474d136f10876a
hhhhjjjj96 <qokkop@ajou.ac.kr>
2020-11-29 19:16:21 +0900

    batch 파일 추가

---
+++ crawling/강남_건대_망원동.bat
@@ -0,0 +1,3 @@
+python insta_crawler.py -t 망원동 -d 1
+python insta_crawler.py -t 가로수길 -d 1
+python insta_crawler.py -t 건대 -d 1
---
+++ crawling/잠실_합정_삼청동_해방촌.bat
@@ -0,0 +1,4 @@
+python insta_crawler.py -t ¿·Ω« -d 1
+python insta_crawler.py -t «’¡§ -d 1
+python insta_crawler.py -t ªÔ√ªµø -d 1
+python insta_crawler.py -t «ÿπÊ√Ã-d 1
