aa3d9eb30ca3a39a615964ee4c4dec66c2191e4c
dhk5844 <dhk5844@ajou.ac.kr>
2020-12-02 21:24:06 +0900

    My 플레이스 예제 추가

--- app/src/main/java/com/posturn/hotplace/MyPlaceActivity.java
+++ app/src/main/java/com/posturn/hotplace/MyPlaceActivity.java
@@ -1,14 +1,22 @@
 package com.posturn.hotplace;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
 public class MyPlaceActivity extends AppCompatActivity {
+    Context context;
+    CardView card1;
+    CardView card2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,27 @@ public class MyPlaceActivity extends AppCompatActivity {
         actionBar.setDisplayShowTitleEnabled(false);
         TextView toolbarTitle = (TextView) findViewById(R.id.toolbar_title);
         toolbarTitle.setText("My 플레이스");
+
+        card1 = (CardView)findViewById(R.id.myfavCard1);
+        card2 = (CardView)findViewById(R.id.myfavCard2);
+
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MarketMainActivity.class);
+                intent.putExtra("placeName","가로수길");
+                startActivity(intent);
+            }
+        });
+        card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MarketMainActivity.class);
+                intent.putExtra("placeName","신림");
+                startActivity(intent);
+            }
+        });
+
     }
 
     //툴바 기능
@@ -36,4 +65,6 @@ public class MyPlaceActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+
 }
